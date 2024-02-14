c4ca5a4dd4b619dbfbe6dee97400883660a581cd
Delphine Nguyen <delphine.nguyen-durandet@sorbonne-nouvelle.fr>
2024-02-14 14:25:05 +0100

    (chore) Update message of error

--- handleChoice.php
+++ handleChoice.php
@@ -33,7 +33,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
         header("Location: ./index.php");
         exit();
     } else {
-        $_SESSION["display"] = "Choice must be either 'edit' or 'delete'";
+        $_SESSION["display"] = "Choice must be either 'edit', 'delete', 'create' or 'search'";
         header("Location: ./index.php");
         exit();
     }
